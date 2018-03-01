47450a9f9996ea1626f0df9abc1b9cd1f8b36439
Carl Gauss <Carl@Friedrich.Gauss>
2018-03-01 14:13:08 +0100

    working adaptive expl

--- Assignment2/code/VanDerPol.py
+++ Assignment2/code/VanDerPol.py
@@ -9,7 +9,7 @@ Created on Mon Feb 19 10:37:39 2018
 import numpy as np
 from scipy.integrate import ode
 import matplotlib.pyplot as plt
-from helperFunctions import *
+import helperFunctions as hF
 
 #font_size = 15
 #plt.rcParams['figure.figsize'] = (15,7)
@@ -23,14 +23,14 @@ from helperFunctions import *
 #plt.rc('figure', titlesize=font_size)   # size of the figure title
 
 
-def VanDerPol (t,x,mu):
+def VanDerPol(t,x,mu):
     # VANDERPOL Implementation of the Van der Pol model
     #
     # Syntax: xdot = VanDerPol(t,x,mu)
     xdot = np.zeros([2, 1])
     xdot[0] = x[1]
     xdot[1] = mu*(1-x[0]*x[0])*x[1]-x[0]
-    return xdot
+    return xdot.T[0]
 
 def JacVanDerPol(t,x,mu):
     # JACVANDERPOL Jacobian for the Van der Pol Equation
@@ -45,15 +45,41 @@ def JacVanDerPol(t,x,mu):
 def VanderPolfunjac(t,x,mu):
     return [VanDerPol(t,x,mu), JacVanDerPol(t,x,mu)]
 
+
+def PreyPredator(t,x,params):
+    # PreyPredator Implementation of the PreyPredator model
+    #
+    # Syntax: xdot = VanDerPol(t,x,params)
+    a = params[0]
+    b = params[1]
+    xdot = np.zeros([2, 1])
+    xdot[0] = a*(1-x[1])*x[0]
+    xdot[1] = -b*(1-x[0])*x[1]
+    return xdot.T[0]
+
+def JacPreyPredator(t,x,params):
+    # JACPREYPREDATOR Jacobian for the Prey Predator Equation
+    #
+    # Syntax: Jac = JacPreyPredator(t,x,params)
+    a = params[0]
+    b = params[1]
+    Jac = np.zeros([2, 2])
+    Jac[0,0] = a*(1-x[1])
+    Jac[1,0] = b*x[1]
+    Jac[0,1] = -a*x[0]
+    Jac[1,1] = -b*(1-x[0])
+    return Jac
+
+def PreyPredatorfunjac(t,x,params):
+    return [PreyPredator(t,x,params), JacPreyPredator(t,x,params)]
+
+
 mu = 10.0
-x0 = np.asarray([2.0, 0.0])
+x0 = np.array([[2.0], [0.0]])
 t0 = 0
 t1 = 5*mu
 
 dt = 0.01
-
-
-t,x = ImplicitEulerFixedStepSize(VanderPolfunjac,t0,t1,5,x0,mu)
 
 r = ode(VanDerPol, JacVanDerPol).set_integrator('vode', method='bdf', order=15) 
 r.set_initial_value(x0, t0).set_f_params(mu).set_jac_params(mu)
@@ -61,19 +87,69 @@ r.set_initial_value(x0, t0).set_f_params(mu).set_jac_params(mu)
 x = [[],[]]
 t = np.arange(t0,t1+dt,dt)
 
-while r.successful() and r.t < t1:
-    xn = r.integrate(r.t+dt)
-    x[0].append(xn[0])
-    x[1].append(xn[1])
+#while r.successful() and r.t < t1:
+#    xn = r.integrate(r.t+dt)
+#    x[0].append(xn[0])
+#    x[1].append(xn[1])
 
 
-plt.plot(t, x[0])
-plt.figure()
-plt.plot(t, x[1])
-plt.figure()
-plt.plot(x[0],x[1])
+#plt.plot(t, x[0])
+#plt.figure()
+#plt.plot(t, x[1])
+#plt.figure()
+#plt.plot(x[0],x[1])
 #plt.show()
 
+[t, x] = hF.ExplicitEulerFixedStepSize(VanDerPol, t0, t1, round((t1-t0)/dt),x0,mu)
+
+plt.plot(t, x[:,0])
+plt.figure()
+plt.plot(t, x[:,1])
+plt.figure()
+plt.plot(x[:,0],x[:,1])
+plt.show()
+
+abstol = 1e-1
+reltol = 1e-1
+
+[t2, x2] = hF.ExplicitEulerAdaptiveStep(VanDerPol,[t0, t1],[2,0],dt,abstol,reltol,mu)
+
+plt.plot(t2, x2[:,0], '-o')
+plt.figure()
+plt.plot(t2, x2[:,1], '-o')
+plt.figure()
+plt.plot(x2[:,0],x2[:,1], '-o')
+plt.show()
 
 #options = odeset(’Jacobian’,@JacVanDerPol,’RelTol’,1.0e-6,’AbsTol’,1.0e-6)
 #[T,X]=ode15s(@VanDerPol,[0 5*mu],x0,options,mu)
+
+a = 1
+b = 1
+x0 = [2,2]
+
+[t, x] = hF.ExplicitEulerFixedStepSize(PreyPredator, t0, t1, round((t1-t0)/dt),x0,[a,b])
+
+plt.plot(t, x[:,0])
+plt.figure()
+plt.plot(t, x[:,1])
+plt.figure()
+plt.plot(x[:,0],x[:,1])
+plt.show()
+
+abstol = 1e-3
+reltol = 1e-3
+
+[t2, x2] = hF.ExplicitEulerAdaptiveStep(PreyPredator,[t0, t1],x0,dt,abstol,reltol,[a,b])
+
+plt.plot(t2, x2[:,0], '-o')
+plt.figure()
+plt.plot(t2, x2[:,1], '-o')
+plt.figure()
+plt.plot(x2[:,0],x2[:,1], '-o')
+plt.show()
+
+
+
+
+
--- Assignment2/code/helperFunctions.py
+++ Assignment2/code/helperFunctions.py
@@ -7,6 +7,7 @@ Created on Thu Feb 22 15:36:20 2018
 """
 import numpy as np
 
+
 def NewtonsMethod(ResidualFunJac, x0, tol, maxit, varargin):
     k = 0
     x = x0
@@ -20,66 +21,127 @@ def NewtonsMethod(ResidualFunJac, x0, tol, maxit, varargin):
     return x
 
 
-def NewtonsMethodODE(FunJac, tk, xk, dt, xinit, tol, maxit, kwargs):
+def NewtonsMethodODE(FunJac, tk, xk, dt, xinit, tol, maxit, varargin):
     k = 0
     t = tk + dt
     x = xinit
-    [f,J] = FunJac(t,x,kwargs)
+    print("----1")
+    print(x)
+    print(t)
+    [f,J] = FunJac(t,x,varargin)
+    print(f)
+    print(dt)
     R = x - f*dt - xk
     I = np.eye(np.size(xk))
     while (k < maxit) and (np.linalg.norm(R,np.inf) > tol):
         k += 1
         dRdx = I - J*dt
         dx = np.linalg.solve(dRdx,R)
+        print("-"*4+"NMODE")
+        print(x)
+        print(dx)
+        print(dRdx)
+        print(R)
         x = x - dx
-        [f,J] = FunJac(t,x,kwargs)
+        [f,J] = FunJac(t,x,varargin)
         R = x - f*dt - xk
     return x
 
-'''
-def ImplicitEulerFixedStepSize(funJac, ta, tb, N, xa, varargin):
+
+def ExplicitEulerFixedStepSize(fun,ta,tb,N,xa,kwargs):
     dt = (tb-ta)/N
-    nx = np.size(xa,1)
-    X = np.zeros([nx,N+1])
-    T = np.zeros([1,N+1])
+    nx = np.size(xa,0)
+    X  = np.zeros([N+1,nx])
+    T  = np.zeros([N+1,1])
+    T[0,:] = np.transpose(ta)
+    X[0,:] = np.transpose(xa)
+    for k in range(N):
+        f = fun(T[k],X[k,:],kwargs)
+        T[k+1,:] = T[k,:] + dt
+        X[k+1,:] = X[k,:] + f*dt;
+    return[T,X]
+
+
+def ExplicitEulerAdaptiveStep(fun,tspan,x0,h0,abstol,reltol,kwargs):
+    maxiter = 10
+    epstol = 0.8
+    facmin = 0.1
+    facmax = 5.0
+    
+    t0 = tspan[0]
+    tf= tspan[1]
+    
+    t = t0
+    h = h0
+    x = np.reshape(x0, (-1, 2))
+    
+    T = np.transpose(t)
+    X = (x)
+    
+    while t < tf or maxiter > 0:
+        maxiter = maxiter - 1
+        if t+h>tf:
+            h = tf-t
+        f = fun(t,x[0],kwargs)
+        
+        AcceptStep = False
+        
+        while not AcceptStep:
+            x1 = x + h*f
+            
+            hm = 0.5*h
+            tm = t + hm
+            xm = x + hm*f
+            
+            fm = fun(tm,xm[0],kwargs)
+            
+            x1hat = xm + hm*fm
+            
+            e = x1hat - x1
+            
+            
+            r = max(max(abs(e)/np.maximum(abstol, abs(x1hat)*reltol)))
+            #print(e)
+            #print(r)
+            AcceptStep = r <= 1
+            #print(AcceptStep)
+            if AcceptStep:
+                t = t+h
+                x = x1hat
+                
+                T = np.append( T, t)
+                X = np.append( X, x ,axis=0 )
+            h = np.maximum(facmin,np.minimum(np.sqrt(epstol/r),facmax))*h
+    return[T,X]
+
+
+def ImplicitEulerFixedStepSize(funJac,ta,tb,N,xa,kwargs):
+    
+    # Compute step size and allocate memory
+    dt = (tb-ta)/N
+    nx = np.size(xa,0)
+    X  = np.zeros([N+1,nx])
+    T  = np.zeros([N+1,1])
+    
     tol = 1.0e-8
     maxit = 100
-    T[:,1] = ta
-    X[:,1] = xa
-    for k in range(1, N):
-        f = fun(T[k],X[:,k],varargin)
-        T[:,k+1] = T[:,k] + dt
-        xinit = X[:,k] + f*dt
-        X[:,k+1] = NewtonsMethodODE(funJac, T[:,k], X[:,k], dt, xinit, tol, maxit, varargin)
-    T = np.transpose(T)
-    X = np.transpose(X)
-    return [T,X]
-    '''
     
-def ImplicitEulerFixedStepSize(funJac,ta,tb,N,xa,kwargs=None):
-
-    # Compute step size and allocate memory
-    dt = (tb-ta)/N;
-    nx = np.size(xa);
-    X  = np.zeros([N+1,nx]);
-    T  = np.zeros([N+1,1]);
-    
-    tol = 10**(-8);
-    maxit = 100;
-
     #Eulers Implicit Method
-    T[0,:] = np.transpose(ta);
-    X[0,:] = np.transpose(xa);
-    for k in range(N-1):
-        [f,j] = funJac(T[k],X[k,:],kwargs);
-        T[k+1,:] = T[k,:] + dt;
-        xinit = X[k,:] + np.transpose(f*dt);
+    T[0,:] = np.transpose(ta)
+    X[0,:] = np.transpose(xa)
+    for k in range(N):
+        [f,j] = funJac(T[k],X[k,:],kwargs)
+        T[k+1,:] = T[k,:] + dt
+        xinit = X[k,:] + np.transpose(f*dt)[0]
         X[k+1,:] = NewtonsMethodODE(funJac,
                                     T[k,:],
                                     X[k,:],
                                     dt,
                                     xinit,
                                     tol,
-                                    maxit,kwargs);
-         
-    return T,X
+                                    maxit,
+                                    kwargs);
+
+    T = np.transpose(T)
+    X = np.transpose(X)
+    return [T,X]
