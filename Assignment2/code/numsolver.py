30b72e58d292ceafecfa5b5996699c625d45cb6a
Kristoffer <linder2411@gmail.com>
2018-03-04 23:04:08 +0100

    krisly

--- Assignment2/code/numsolver.py
+++ Assignment2/code/numsolver.py
@@ -6,181 +6,194 @@ This is a temporary script file.
 """
 import numpy as np
 
+def func_counter(func_counter=0):
+    func_counter += 1
+    return func_counter
+
 class numerical_solvers(object):
-        
-    def NewtonsMethod(self,ResidualFunJac, x0, tol, maxit, varargin):
-        self.k = 0
-        self.x = x0
-        self.R,self.dRdx = ResidualFunJac(self.x,varargin)
-        while (self.k < maxit) and (np.linalg.norm(self.R,np.inf) > tol):
-            self.k += 1
-            self.dx = np.linalg.solve(self.dRdx,self.R)
-            self.x = self.x - self.dx
-            self.R,self.dRdx = ResidualFunJac(self.t,self.x,varargin)
-        return self.x   
+    t      = np.array([0,100])
+    x0     = np.array([0,0])
+    absTol = 10**(-6)
+    relTol = 10**(-3)
+    epsTol = 0.8
+    maxit  = 100
+    R      = 0
+    k      = 0
+    param  = 0
+    dt     = 0.001
+    facmin = 0.1
+    facmax = 5
+    seed   = 3
     
-    def NewtonsMethodODE(self,FunJac, tk, xk, dt, xinit, tol, maxit, kwargs):
-        self.k = 0
-        self.t = tk + dt
-        self.x = xinit
-        self.f,self.J = FunJac(self.t,self.x,kwargs)
-        self.R = self.x - self.f*dt - xk
-        self.I = np.eye(np.size(xk))
-        while (self.k < maxit) and (np.linalg.norm(self.R,np.inf) > tol):
-            self.k += 1
-            self.dRdx = self.I - self.J*dt
-            self.dx = np.linalg.solve(self.dRdx,np.transpose(self.R))
-#            self.dx = self.dx[0]
-            self.x = self.x - self.dx
-            self.f,self.J = FunJac(self.t,self.x,kwargs)
-            self.R = self.x - self.f*dt - xk
-        return self.x
+    def NewtonsMethod(self,rjf,x0):
+        k        = self.k
+        x        = x0
+        t        = self.t
+        res,dRdx = rjf(t,x,self.param)
+        
+        while (k < self.maxit) and (np.linalg.norm(res,np.inf) >
+                                         self.absTol):
+            k        += 1
+            dx       = np.linalg.solve(dRdx,res)
+            x        = x - dx
+            res,dRdx = rjf(t,x,self.param)
+        return x   
+    
+    def NewtonsMethodODE(self,rjf, tk,x, xk,dt):
+        k   = self.k
+        t   = tk + dt
+        f,J = rjf(t,x,self.param)
+        res = x - f*dt - xk
+        I   = np.eye(np.size(xk))
+        
+        while (k < self.maxit) and (np.linalg.norm(res,np.inf) > self.absTol):
+            k    += 1
+            dRdx = I - J*dt
+            dx   = np.linalg.solve(dRdx,np.transpose(res))
+            x    = x - dx
+            f,J  = rjf(t,x,self.param)
+            res  = x - f*dt - xk
+        return x
     
     def VanDerPol(self,t,x,mu):
-        # VANDERPOL Implementation of the Van der Pol model
-        #
-        # Syntax: xdot = VanDerPol(t,x,mu)
-        self.mu=mu
-        self.t=t
-        self.x=x
-        self.xdot = np.zeros([2, 1])
-        self.xdot[0] = self.x[1]
-        self.xdot[1] = self.mu*(1-self.x[0]*self.x[0])*self.x[1]-self.x[0]
-        return self.xdot.T[0]
+        xdot    = np.zeros([2, 1])
+        xdot[0] = x[1]
+        xdot[1] = mu*(1-x[0]*x[0])*x[1]-x[0]
+        return xdot.T[0]
 
     def JacVanDerPol(self,t,x,mu):
-        # JACVANDERPOL Jacobian for the Van der Pol Equation
-        #
-        # Syntax: Jac = JacVanDerPol(t,x,mu)
-        self.x =x
-        self.mu=mu
-        self.t=t
-        self.Jac = np.zeros([2, 2])
-        self.Jac[1,0] = -2*self.mu*self.x[0]*self.x[1]-1.0
-        self.Jac[0,1] = 1.0
-        self.Jac[1,1] = self.mu*(1-self.x[0]*self.x[0])
-        return self.Jac
+        Jac      = np.zeros([2, 2])
+        Jac[1,0] = -2*mu*x[0]*x[1]-1.0
+        Jac[0,1] = 1.0
+        Jac[1,1] = mu*(1-x[0]*x[0])
+        return Jac
 
     def VanderPolfunjac(self,t,x,mu):
-        self.t=t
-        self.x=x
-        self.mu=mu
-        return self.VanDerPol(self.t,
-                              self.x,
-                              self.mu),self.JacVanDerPol(self.t,
-                                                         self.x,
-                                                         self.mu)
+        return self.VanDerPol(t,x,mu),self.JacVanDerPol(t,x,mu)
     
-    def ImplicitEulerFixedStepSize(self,funJac,ta,tb,N,xa,kwargs=None):
-
-    # Compute step size and allocate memory
-        self.dt = (tb-ta)/N;
-        self.nx = np.size(xa);
-        self.X  = np.zeros([N+1,self.nx]);
-        self.T  = np.zeros([N+1,1]);
-    
-        self.tol = 10**(-8);
-        self.maxit = 100;
-
-    #Eulers Implicit Method
-        self.T[0,:] = ta;
-        self.X[0,:] = xa;
-        for k in range(N):
-            self.f,self.j = funJac(self.T[k],self.X[k,:],kwargs);
-#            self.f=self.f[0]
-            self.T[k+1,:] = self.T[k,:] + self.dt;
-            self.xinit = self.X[k,:] + np.transpose(self.f*self.dt);
-            self.X[k+1,:] = self.NewtonsMethodODE(funJac,
-                                        self.T[k,:],
-                                        self.X[k,:],
-                                        self.dt,
-                                        self.xinit,
-                                        self.tol,
-                                        self.maxit,
-                                        kwargs);
-         
-        return self.T,self.X
-   
-    def ImplicitEulerAdaptiveStepSize(self,fun,ta,tb,xa,h,absTol,relTol,
-                                      epstol,kwargs):
-        self.t=ta
-        self.h = h
-        self.x = xa
-        self.facmin = 0.1
-        self.facmax = 5
-        self.T = np.array([self.t])
-        self.X = list()
-        self.X += [self.x]
-        self.ss =np.array([self.h])
-        self.epstol = epstol
-        self.absTol = absTol
-        self.relTol = relTol
-        self.tb=tb
-        self.kwargs = kwargs
-        #print(type(self.X))
+    def ImplicitEulerFixedStepSize(self,rjf):
+        dt     = self.dt
+        N      = np.int32(round((self.t[1]-self.t[0])/self.dt))
+        nx     = np.size(self.x0);
+        X      = np.zeros([N+1,nx]);
+        T      = np.zeros([N+1,1]);
+        T[0,:] = self.t[0];
+        X[0,:] = self.x0;
+        k      = self.k
         
-        while self.t < self.tb:
-            if(self.t+self.h>self.tb):
-                self.h = self.tb-self.t
-                
-            #print(self.f)
-            self.t =self.t+self.h
-            self.AcceptStep = False
+        for k in range(N):
+            f,j      = rjf(T[k],X[k,:],self.param);
+            T[k+1,:] = T[k,:] + dt;
+            X[k+1,:] = self.NewtonsMethodODE(rjf,T[k,:],X[k,:],X[k,:],dt);
+        return T,X
+   
+    def ImplicitEulerAdaptiveStepSize(self,fun,absTol_as = None):
 
-            while not self.AcceptStep:
-                #print(self.x1)                
-                self.x1 = self.NewtonsMethodODE(fun,
-                                                self.t,
-                                                self.x,
-                                                self.h,
-                                                self.x,
-                                                self.absTol,
-                                                100,
-                                                self.kwargs)
+        iea_t        = self.t[0]
+        tend         = self.t[1]
+        x1           = self.x0
+        dt           = self.dt
+        T            = np.zeros([1,],dtype=np.float64)
+        T[0]         = iea_t
+        X            = np.array([x1[0],x1[1]], dtype=np.float64)
+        ss           = np.zeros([1,],dtype=np.float64)
+        ss[0]        = self.dt
+        accept       = 0
+        nran         = 0
+        nreject      = 0
+        t1           = iea_t
+        
+        while t1 < tend:
+            if(iea_t+dt>tend):
+                dt = tend-iea_t
+
+            AcceptStep = False
+
+            while not AcceptStep:    
+                iea_t =iea_t+dt
+                x1    = self.NewtonsMethodODE(fun,iea_t,x1,x1,dt)
                 
-                self.hm = 0.5*self.h
-                self.tm = self.t + self.hm
-                self.x1hat = self.NewtonsMethodODE(fun,
-                                                   self.tm,
-                                                   self.x,
-                                                   self.hm,
-                                                   self.x,
-                                                   self.absTol,
-                                                   100,
-                                                   self.kwargs)
+                hm = 0.5*dt
+                tm = t1 + hm
+                x1hat = self.NewtonsMethodODE(fun,tm,x1,x1,dt)
                 
-                self.x1hat = self.NewtonsMethodODE(fun,
-                                                   self.t,
-                                                   self.x1hat,
-                                                   self.hm,
-                                                   self.x1hat,
-                                                   self.absTol,
-                                                   100,
-                                                   self.kwargs)
+                x1hat = self.NewtonsMethodODE(fun,iea_t,x1hat,x1hat,dt)
     
-#                print(self.x1hat)
-                self.e = self.x1hat - self.x1
+                e = x1hat - x1
+                if absTol_as!=None:
+                    r = np.max(np.abs(e)/np.max([absTol_as,
+                           np.max(np.abs(x1hat)*self.relTol)]))
+                else:
+                    r = np.max(np.abs(e)/np.max([self.absTol,
+                           np.max(np.abs(x1hat)*self.relTol)]))
+    
+                AcceptStep = (r <= 1)
+                
+                if AcceptStep:
+                    x  = x1hat
+                    T  = np.append(T,iea_t)
+                    X  = np.append(X,x)
+                    ss = np.append(ss,dt)
+                    accept  = func_counter(accept)
+                    t1 = iea_t
+                
+                dt = np.max([self.facmin,
+                          np.min([np.sqrt(self.epsTol/np.float64(r)),
+                                          self.facmax])])*dt
+    
+                nran = func_counter(nran)
+                
+        nreject = nran - accept   
+        return T,X.reshape((len(T),2)),ss,nreject
+ 
+    def ScalarStdWienerProcess(self,T,N,Ns):
+        np.random.seed(self.seed)
+        dt  = T/N
+        dW  = np.sqrt(dt)*np.random.randn(Ns,N)
+        W   = np.append(np.zeros([Ns,1]), np.cumsum(dW,1), axis=1)
+        Tw  = np.arange(0,T+dt/2,dt)
+        return W,Tw,dW
 
-                self.r = np.max(np.abs(self.e)/np.max([self.absTol,
-                                np.max(np.abs(self.x1hat)*self.relTol)]))
-  #              print(self.x1hat,t)
-  #              print(np.abs(self.e),self.r,t)
-                self.AcceptStep = (self.r <= 1)
-               # print("Timestep: {} and r value: {}".format(t,self.r))
-                if self.AcceptStep:
-                    self.x = self.x1hat
-                    self.T = np.append(self.T,self.t)
-               #     print(self.x)
-              #      print(type(self.X))
-                    self.X += [self.x]
-                    self.ss =np.append(self.ss,self.h)
-                    
-                self.h = np.max([self.facmin,
-                                np.min([np.sqrt(self.epstol/np.float64(self.r)),
-                                        self.facmax])])*self.h
-                                
-        return np.asarray(self.T),np.asarray(self.X),self.ss
-                                
+
+    def SDEeulerExplicitExplicit(self,ffun,gfun,T,x0,W):
+        N      = np.size(T)-1
+        nx     = np.size(x0)
+        X      = np.zeros([nx,N+1])
+        X[:,0] = x0
+        for k in range(0,N):
+            dt       = T[k+1]-T[k]
+            dW       = W[k+1]-W[k]
+            f        = ffun(T[k],X[:,k],self.param)
+            g        = gfun(T[k],X[:,k],self.param)
+            X[:,k+1] = X[:,k] + f*dt + g*dW
+        return X
+
+    def SDEeulerImplicitExplicit(self,ffun,gfun,T,x0,dW):
+        N      = np.size(T)-1
+        nx     = np.size(x0)
+        X      = np.zeros([nx,N+1])
+        X[:,0] = x0
+        for k in range(0,N):
+            dt       = T[k+1]-T[k]
+            f        = ffun(T[k],X[:,k],self.param)
+            g        = gfun(T[k],X[:,k],self.param)
+            psi      = X[:,k] + g*dW[:,k]
+            X        = psi + f*dt
+            #X[:,k+1] = self.NewtonsMethodNewton(ffun+gfun,X)
+            X[:,k+1] = self.NewtonsMethodODE()
+        return X
+
+    def LangevinDrift(self,t,x,p):
+        x     = x
+        lamda = p[0]
+        f     = lamda*x
+        return f
+
+    def LangevinDiffusion(self,t,x,p):
+        sigma = p[1]
+        g     = sigma
+        return g
+
                 
             
         
--- Assignment2/code/test_script_2.py
+++ Assignment2/code/test_script_2.py
@@ -10,7 +10,7 @@ import numpy as np
 from scipy.integrate import ode
 import matplotlib.pyplot as plt
 
-
+# Sets Plotting parameters
 font_size = 15
 plt.rcParams['figure.figsize'] = (15,7)
 plt.rc('font',   size=font_size)       # controls default text sizes
@@ -21,58 +21,50 @@ plt.rc('ytick',  labelsize=font_size)  # fontsize of the tick labels
 plt.rc('legend', fontsize=font_size)   # legend fontsize
 plt.rc('figure', titlesize=font_size)  # # size of the figure title
 
-numsolv1 = numerical_solvers()
-numsolv2 = numerical_solvers()
-#vander_pol = numsolv1(1,3,5)
-#print(sol1,sol2)
-mu = 1
-x0 = np.array([2.0,0.0])
-t0 = 0
-t1 = 10
-dt = 0.01
-t = np.arange(t0,t1+dt,dt)
-absTol=10**(-6)
-relTol=10**(-4)
-epstol=0.8
+# Setting up solver parameters
+mu              = 1
+x0              = np.array([2.0,0.0])
+tend            = 200
+numsolv1        = numerical_solvers()
+numsolv1.param  = mu
+numsolv1.x0     = x0
+numsolv1.maxit  = 100
+numsolv1.t      = np.array([0,tend])
+numsolv1.dt     = 0.01
+t               = np.arange(0,tend+0.01,0.01)
+numsolv1.absTol = 10**(-8)
+numsolv1.relTol = 10**(-3)
+numsolv1.epstol = 0.8
+absTol_as       = 10**(-4) 
 
-sol_T,sol_X = numsolv1.ImplicitEulerFixedStepSize(numsolv1.VanderPolfunjac,
-                                    t0,
-                                    t1, 
-                                    round((t1-t0)/dt),
-                                    x0.T,
-                                    mu)
+# Solving using the Implicit fixed step size Euler method
+sol_T,sol_X = numsolv1.ImplicitEulerFixedStepSize(numsolv1.VanderPolfunjac)
 
-a_sol_T,a_sol_X,a_ss = numsolv2.ImplicitEulerAdaptiveStepSize(numsolv1.VanderPolfunjac,
-                                                              t0,
-                                                              t1,
-                                                              x0.T,
-                                                              dt,
-                                                              absTol,
-                                                              relTol,
-                                                              epstol,
-                                                              mu)
-
+# Solving using the adaptive Implicit Euler
+a_sol_T,a_sol_X,a_ss,reject = numsolv1.ImplicitEulerAdaptiveStepSize(
+                                                    numsolv1.VanderPolfunjac,
+                                                    absTol_as)
+# Setting up scipy solver
 r = ode(numsolv1.VanDerPol,
         numsolv1.JacVanDerPol).set_integrator('vode',
                                               method='bdf',
                                               order=15)
-
-r.set_initial_value(x0, t0).set_f_params(mu).set_jac_params(mu)
-
+r.set_initial_value(x0, 0).set_f_params(mu).set_jac_params(mu)
 x = [[],[]]
 
-while r.successful() and r.t < t1:
-    xn = r.integrate(r.t+dt)
+# Solving using the scipy solver
+while r.successful() and r.t < tend:
+    xn = r.integrate(r.t+0.01)
     x[0].append(xn[0])
     x[1].append(xn[1])
 
-
-plt.plot(t, x[0],label='Scipy solver')
+# Plotting the results
+plt.plot(t[:len(x[0])], x[0],label='Scipy solver')
 plt.plot(sol_T, sol_X[:,0],label='Implicit Euler fixed step')
 plt.plot(a_sol_T, a_sol_X[:,0],label='Implicit Euler adaptive step')
 plt.legend(loc='lower left')
 plt.figure()
-plt.plot(t, x[1],label='Scipy Solver')
+plt.plot(t[:len(x[1])], x[1],label='Scipy Solver')
 plt.plot(sol_T, sol_X[:,1],label='Implicit Euler')
 plt.plot(a_sol_T, a_sol_X[:,1],label='Implicit Euler adaptive step')
 plt.legend(loc='lower left')
