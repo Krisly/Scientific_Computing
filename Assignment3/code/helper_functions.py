45fee862597e4221214ff0b46e735cac423a4ce6
Linder <linder2411@gmail.com>
2018-04-15 23:11:53 +0200

    Missing

--- Assignment3/code/helper_functions.py
+++ Assignment3/code/helper_functions.py
@@ -1,7 +1,6 @@
 import numpy as np
 import scipy as sp
 import scipy.sparse
-from numba import jit
 
 def poisson5(m):
 	e = np.repeat(1,m)
@@ -17,4 +16,14 @@ def poisson9(m):
 	I = sp.sparse.spdiags(np.array([-1/2*e, e ,-1/2*e]), np.array([-1, 0 ,1]), m, m)
 	A = 1/6*(m+1)**2*(sp.sparse.kron(I,S)+sp.sparse.kron(S,I))
 	return A
+
+def rhs_fun(fun,x,y):
+	rhs = fun(x,y)
+	return rhs
+
+def func(x,y):
+	return np.sin(4*np.pi*(x+y))+np.cos(4*np.pi*x*y)
+
+xv, yv = np.meshgrid(np.arange(0,1,0.4), np.arange(0,1,0.4), sparse=False, indexing='ij')
+print(rhs_fun(func,xv,yv))
 
