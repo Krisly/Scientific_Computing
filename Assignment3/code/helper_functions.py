71722eafbe117a82d129f5da0898db4e5d6f88b3
krisly <linder2411@gmail.com>
2018-04-22 23:02:18 +0200

    Updated

--- Assignment3/code/helper_functions.py
+++ Assignment3/code/helper_functions.py
@@ -10,6 +10,75 @@ from scipy import signal
 from scipy import misc
 
 
+def cgls( A,b,x0 = 'None',maxIter=1000):
+    '''
+     The cgls function use an iterative method to solve a Linear system of the form
+     Ax=b
+    
+     INPUT ARGUMENTS
+       A       : Is the system matrix 
+       b       : Is the vector containing the information of the right-hand side
+       maxIter : The number of iterations for the algoritmen to tun through
+       xk      : A start guess for the algoritment if not provided the zero vector will be 
+                 assumed!!
+
+     OUTPUT
+        X : Is a matrix containing a soltion for each iteration hence a matrix
+            with as many rows as A and colums as maxIter
+     
+
+    # Sets x0 if not provided (as the zero vector)
+    if x0 == 'None':
+        x0 = np.zeros((A.shape[1],1))
+    '''     
+    # Check data type of the System-Matrix
+    if not isinstance(A,np.matrix):
+        print('Wrong Data Type: A must be a matrix')
+        raise
+    
+    # Checks thats the right-hand side is a vectord
+    if not isinstance(b,np.matrix):
+        error('Wring Data Type: b must be a vector (np.matrix)')
+        raise  
+    
+    # Check the maximum number of iterations
+    if maxIter < 1:
+        error('Maximum iterations cannot be less than one')
+        raise
+    
+    # Makes sure the right hand side is a column vector 
+    if b.shape[0]<b.shape[1]:
+       b = b.T
+
+    # Makes sure the initial guess is a column vector 
+    if x0.shape[0]<x0.shape[1]:
+        x0 = x0.T
+    
+    C = A.shape[0]               # Gets the number of columns of A
+    sol = np.zeros((C,maxIter))  # Preallocates the matrix X
+    
+    r = b - A*x0
+    p = A.T*r
+    norm = p.T*p;
+    
+    for i in  range(maxIter): 
+        # Updates the initial guess, x0 and r.
+        temp_p = A*p
+        ak = (norm/(temp_p.T*temp_p)).item()
+        x0 = x0+ak*p
+        r = r-ak*temp_p
+        p2 = A.T*r;
+    
+        # Updates the vector d
+        nn = p2.T*p2
+        beta = (nn/norm).item()
+        norm = nn
+        p = p2 + beta*p;
+        
+        # Saves the solution to X
+        sol[:,i]=x0.T;
+    return sol
+
 def poisson5(m):
 	# Calculates the 5-point Laplacian system matrix A
 	e = np.repeat(1,m)
--- Assignment3/code/poisson_test_script.py
+++ Assignment3/code/poisson_test_script.py
@@ -5,24 +5,24 @@ from scipy import signal
 from mpl_toolkits.mplot3d import Axes3D
 
 # Solving the system for the test function 2
-m     = 3
+m     = 50
 
 # Structuring elements
 lp9          = np.array([[1,4,1],[4,-20,4],[1,4,1]]) 													# 9-point Laplace structuring element
 lp5          = np.array([[0,1,0],[1,-4,1],[0,1,0]])  													# 5-point Laplace structuring element
-print(lp5)
+#print(lp5)
 # Domain Discritization
 h            = 1/(m+1)				   				 													# Step size
 x            = np.linspace(0, 1, m+2)																	# X range
 y            = np.linspace(0, 1, m+2)				 													# Y range
 X, Y         = np.meshgrid(x, y)					 													# Domain meshgrid
 f_lp5        = (h**2/12)*signal.convolve2d(u_excact_0(Y,X),lp5,mode='valid')					    	# 5-point laplacian of right-hand side
-print(f_lp5)
-print("")
+#print(f_lp5)
+#print("")
 
 # Boundary conditions
 g            = u_excact_0(X,Y)																			# Full rhs
-print(g[1:-1,1:-1])
+#print(g[1:-1,1:-1])
 g[1:-1,1:-1] = 0																						# Setting interior to zero
 g            = (1/(6*h**2))*signal.convolve2d(g,lp9,mode='valid') 										# Final boundary condition by convulution
 
@@ -34,16 +34,16 @@ ax    = fig.gca(projection='3d')
 ax.plot_surface(X[1:-1,1:-1],
 				Y[1:-1,1:-1],
 				u.reshape(m,m), 
-				rstride=8, 
-				cstride=8, 
+				rstride=3, 
+				cstride=3, 
 				alpha=0.7,
 				label='Solved')
 
 ax.plot_surface(X[1:-1,1:-1], 
 				Y[1:-1,1:-1], 
 				u_excact_0(X[1:-1,1:-1],Y[1:-1,1:-1]),
-				rstride=8, 
-				cstride=8, 
+				rstride=3, 
+				cstride=3, 
 				alpha=0.4,
 				color='green',
 				label='True')
